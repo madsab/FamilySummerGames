144c7f7631ac018aff8f864da3d9b28067e147fe
madsab <mads.barnes@hotmail.com>
2024-06-27 21:01:23 +0200

    setup base layout

---
+++ app/components/BottomNavbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BottomNavbar = () => {
+  return (
+    <div className="w-full fixed bg-slate-800 bottom-0 p-3 flex justify-around">
+      <div>Regler</div>
+      <div>Hint</div>
+      <div>Ulemper</div>
+    </div>
+  );
+};
+
+export default BottomNavbar;
