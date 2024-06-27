20122cdd2879c71181a6a0c9dd5ab36509d0af0b
madsab <mads.barnes@hotmail.com>
2024-06-27 22:26:25 +0200

    navbar

--- app/components/BottomNavbar.tsx
+++ app/components/BottomNavbar.tsx
@@ -1,11 +1,21 @@
+"use client";
 import React from "react";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/navigation";
 
 const BottomNavbar = () => {
+  const router = useRouter();
   return (
-    <div className="w-full fixed bg-slate-800 bottom-0 p-3 flex justify-around">
-      <div>Regler</div>
-      <div>Hint</div>
-      <div>Ulemper</div>
+    <div className="w-full fixed bg-slate-800 bottom-0 px-3 py-6 flex justify-around items-center">
+      <button onClick={() => router.push("/shop")}>
+        <Icon icon={"solar:money-bag-outline"} className=" size-7" />
+      </button>
+      <button onClick={() => router.push("/")}>
+        <Icon icon={"akar-icons:home-alt1"} className=" size-7" />
+      </button>
+      <button>
+        <Icon icon={"charm:person"} className=" size-7" />
+      </button>
     </div>
   );
 };
