c88e47a79a39471f4ce542e6176ed36f97b7d5a2
madsab <mads.barnes@hotmail.com>
2024-06-28 13:39:52 +0200

    moved to (page)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../globals.css";
+
+export default function SignupLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="w-full h-screen flex justify-center items-center">
+        <main className="bg-slate-600 p-5 rounded-md ">{children}</main>
+      </body>
+    </html>
+  );
+}
--- app/(page)/layout.tsx
+++ app/(page)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import cn from "classnames";
 import BottomNavbar from "./components/BottomNavbar";
 
