e5fd1f3fccdd21c936ca8a35ca6329457cd4c9a1
madsab <mads.barnes@hotmail.com>
2024-06-29 02:02:07 +0200

    session types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,30 @@
+import NextAuth from 'next-auth';
+import 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    user: User;
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string | null;
+    emailVerified: Date | null;
+    hashedPassword: string | null;
+    image: string | null;
+    familyName: string;
+    money: number;
+    role: $Enums.Role;
+    createdAt: Date;
+    updatedAt: Date;
+    // add other custom fields if needed
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user: User
+    // add other custom fields if needed
+  }
+}
