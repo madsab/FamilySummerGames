652147f0518a082702ab1a950be280238ccbdc2f
madsab <mads.barnes@hotmail.com>
2024-06-29 19:14:15 +0200

    show user coins + styling

--- app/(page)/shop/page.tsx
+++ app/(page)/shop/page.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-import ShopItem from "../components/ShopItem";
+import ShopItem from "../../components/ShopItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/config/auth/authOptions";
+import { redirect } from "next/navigation";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Shop from "../pages/Shop";
 
-const ShopPage = () => {
+const ShopPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect("/signin");
+  }
+
   return (
-    <div className="h-full w-full flex flex-col justify-evenly items-center">
-      <ShopItem title="Hint" description="lorem" shopitems={[{ id: 1, name: "somtehing", price: 2000 }]} />
-      <ShopItem title="Hint" description="lorem" shopitems={[{ id: 1, name: "somtehing", price: 2000 }]} />
+    <div className="h-full w-full">
+      <div className="mx-5 flex flex-col items-center">
+        <p className="text-2xl">Butikk</p>
+        <hr className="text-white w-full h-1" />
+      </div>
+      <div className="mt-3 flex justify-center items-center">
+        <Icon icon={"fluent-emoji:coin"} className="size-8" />
+        <p>: {session.user.money}</p>
+      </div>
+      <Shop />
     </div>
   );
 };
