0855a876801d533a41533f5fef995380fb96ea1b
madsab <mads.barnes@hotmail.com>
2024-06-29 14:09:21 +0200

    add: dark background on active

--- app/(page)/components/BottomNavbar.tsx
+++ app/(page)/components/BottomNavbar.tsx
@@ -2,20 +2,40 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import { useRouter } from "next/navigation";
+import cn from "classnames";
 
 const BottomNavbar = () => {
   const router = useRouter();
+  const [active, setActive] = React.useState(1);
   return (
-    <div className="w-full fixed bg-slate-800 bottom-0 px-3 py-6 flex justify-around items-center">
-      <button onClick={() => router.push("/shop")}>
-        <Icon icon={"solar:money-bag-outline"} className=" size-7" />
-      </button>
-      <button onClick={() => router.push("/")}>
-        <Icon icon={"akar-icons:home-alt1"} className=" size-7" />
-      </button>
-      <button>
-        <Icon icon={"charm:person"} className=" size-7" />
-      </button>
+    <div className="w-full fixed bg-slate-800 bottom-0 h-20 flex  items-center">
+      <div className={cn("flex-1 flex justify-center h-full", active === 0 && " bg-slate-900")}>
+        <button
+          onClick={() => {
+            router.push("/shop"), setActive(0);
+          }}
+        >
+          <Icon icon={"solar:money-bag-outline"} className=" size-7" />
+        </button>
+      </div>
+      <div className={cn("flex-1 flex justify-center h-full", active === 1 && " bg-slate-900")}>
+        <button
+          onClick={() => {
+            router.push("/"), setActive(1);
+          }}
+        >
+          <Icon icon={"akar-icons:home-alt1"} className=" size-7" />
+        </button>
+      </div>
+      <div className={cn("flex-1 flex justify-center h-full", active === 2 && " bg-slate-900")}>
+        <button
+          onClick={() => {
+            router.push("/code"), setActive(2);
+          }}
+        >
+          <Icon icon={"mdi:qrcode-scan"} className=" size-7" />
+        </button>
+      </div>
     </div>
   );
 };
