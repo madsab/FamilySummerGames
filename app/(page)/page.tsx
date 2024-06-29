358462e63d47fc5cc4e926bf1a13407598a6f5f1
madsab <mads.barnes@hotmail.com>
2024-06-29 19:13:44 +0200

    show name

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -1,12 +1,19 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/config/auth/authOptions";
+import { redirect, useRouter } from "next/navigation";
+import TestPage from "./pages/TestPage";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   console.log(session);
 
   if (!session?.user) {
-    return <div>Not logged in</div>;
+    redirect("/signin");
   }
-  return <div>Hello, {session.user.money}</div>;
+  return (
+    <div>
+      Hello, {session.user.name}
+      <TestPage />
+    </div>
+  );
 }
