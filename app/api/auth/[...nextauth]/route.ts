ceb876ae509b71e3aa4c31b743441663bf2785d5
madsab <mads.barnes@hotmail.com>
2024-06-29 11:15:17 +0200

    errorfix: wrongly exported "authOptions"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,4 @@ import { authOptions } from '@/lib/config/auth/authOptions';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST};
