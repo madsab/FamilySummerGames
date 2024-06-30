b41e270baa6dd62c8fe658e3c6abbe232de40c6a
madsab <mads.barnes@hotmail.com>
2024-06-30 23:38:07 +0200

    styling

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,16 +1,21 @@
 "use client";
 import React, { FC } from "react";
+import cn from "classnames";
 
 interface ButtonProps {
   text: string;
+  className?: string;
   onClick: () => void;
 }
-const Button: FC<ButtonProps> = ({ text, onClick }) => {
+const Button: FC<ButtonProps> = ({ text, onClick, className }) => {
   return (
     <button
       onClick={onClick}
       type="button"
-      className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className={cn(
+        "rounded bg-indigo-600 px-4 py-3 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+        className
+      )}
     >
       {text}
     </button>
