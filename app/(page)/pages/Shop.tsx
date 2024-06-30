a511dddf00c060361252b2b813a8cc101fd37810
madsab <mads.barnes@hotmail.com>
2024-06-30 20:44:28 +0200

    Shop layout + buying disadvantages

--- app/(page)/pages/Shop.tsx
+++ app/(page)/pages/Shop.tsx
@@ -1,26 +1,118 @@
-import React, { useState } from "react";
+"use client";
+import React, { FC } from "react";
 import ShopItem from "../../components/ShopItem";
 import Select from "@/app/components/Select";
 import Ulemper from "@/app/utils/disadvantage.json";
-import { getAllUsers } from "@/lib/db/db";
-import Disadvantages from "@/app/components/Disadvantages";
-import { User, Role } from "@prisma/client";
+import { User } from "@prisma/client";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { useSession } from "next-auth/react";
+import addPurchase, { PurchaseData } from "@/app/actions/addPurchase";
+import { toast } from "react-toastify";
 
-const Shop = async () => {
-  const players = (await getAllUsers()) as User[];
+interface ShopProps {
+  players: User[];
+}
 
+const Shop: FC<ShopProps> = ({ players }) => {
+  const session = useSession();
+  const nullData = {
+    type: null,
+    text: null,
+    to: null,
+    from: null,
+    price: null,
+  };
+  const [formData, setFormData] = React.useState<PurchaseData>(nullData);
+
+  if (!session.data?.user) {
+    return <div>Loading...</div>;
+  }
+
+  const buyDisadvantage = async () => {
+    console.log(formData);
+    const { data, error } = await addPurchase(formData);
+    if (error) {
+      toast.error(error);
+    } else {
+      toast.success(
+        <div className="flex items-center text-sm ">
+          <span className="flex items-center">
+            Kjøpt &apos;{data?.text}&apos; for <Icon icon={"fluent-emoji:coin"} />
+            {data?.price} mynter!
+          </span>
+        </div>
+      );
+      setFormData(nullData);
+    }
+  };
   return (
     <div className="w-full h-5/6">
       <div className="flex flex-wrap h-full p-4 gap-3">
         <ShopItem
+          onCancel={() => setFormData(nullData)}
+          onConfirm={() => buyDisadvantage()}
           title="Ulemper"
           icon="tabler:alert-triangle"
           description="Kjøp ulemper andre spillere må ha under neste aktivitet"
         >
-          <Disadvantages players={players} />
+          <div>
+            <div>
+              <fieldset className="mb-[15px] flex items-center gap-5">
+                <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
+                  Type:
+                </label>
+                <Select
+                  title="Ulemper"
+                  placeholder="Velg Ulempe"
+                  items={Ulemper}
+                  onChange={(value) => {
+                    setFormData((prevValue) => ({
+                      ...prevValue,
+                      text: value,
+                      type: "ulempe",
+                      from: session.data.user.email,
+                    }));
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      price: Ulemper.find((item) => item.title === value)?.price || 0,
+                    }));
+                  }}
+                />
+              </fieldset>
+              <fieldset className="mb-[15px] flex items-center gap-5">
+                <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="username">
+                  Spiller:
+                </label>
+                <Select
+                  title="Spiller"
+                  placeholder="Velg Spiller"
+                  items={players.map((player) => player.name)}
+                  onChange={(value) => setFormData((prevValue) => ({ ...prevValue, to: value + "@fsg.com" }))}
+                />
+              </fieldset>
+              <div className="mt-8 flex items-center space-x-4">
+                <p>Sum:</p>
+                {formData.price && (
+                  <p className="flex items-center">
+                    <Icon icon={"fluent-emoji:coin"} /> {formData.price}
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
         </ShopItem>
-        <ShopItem title="Hint" description="lorem" />
-        <ShopItem title="Kjøp spiller" description="lorem" />
+        <ShopItem
+          onCancel={() => setFormData(nullData)}
+          onConfirm={() => null}
+          title="Hint"
+          description="Kjøp hint til neste spill"
+        />
+        <ShopItem
+          onCancel={() => setFormData(nullData)}
+          onConfirm={() => null}
+          title="Kjøp spiller"
+          description="lorem"
+        />
       </div>
     </div>
   );
