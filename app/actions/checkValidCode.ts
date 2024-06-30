030f7ac747f7488ab1c9e7e164873cf7bbfc8d78
madsab <mads.barnes@hotmail.com>
2024-06-30 23:37:48 +0200

    add: check one-time codes

--- app/actions/checkValidCode.ts
+++ app/actions/checkValidCode.ts
@@ -1,7 +1,14 @@
 
 function checkValidCode(code: string): number {
+    // Check if code is an admin code
     if (code.startsWith("abcx")){
         return parseInt(code.split("x")[1])
+    }
+
+    //Check if code is a user one-time code
+    if (code.includes("yzx")&& code.indexOf("yzx") === 1){
+        const validNumbers = parseInt(code.split("yzx")[0])
+        return parseInt(code.split("yzx")[1].substring(0, validNumbers))
     }
     return 0;
 }
