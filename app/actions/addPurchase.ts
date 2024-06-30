4546e8e99700faf7195f89034e2dd3bfc2548726
madsab <mads.barnes@hotmail.com>
2024-06-30 20:58:47 +0200

    add purchase to db

---
+++ app/actions/addPurchase.ts
@@ -0,0 +1,67 @@
+"use server"
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export interface PurchaseData {
+    type: string | null;
+    text: string | null;
+    price: number | null;
+    to?: string | null;
+    from: string | null;
+    createdAt?: Date;
+}
+
+interface PurchaseResult {
+    data?: PurchaseData;
+    error?: string;
+}
+
+async function addPurchase(formData: PurchaseData): Promise<PurchaseResult> {
+
+    //Check if the form data is valid
+    if (!formData.text || !formData.to || !formData.price || !formData.from || !formData.type) {
+        return { error: "Vennligst fyll ut alle feltene" };
+    }
+
+
+try {
+    const purchasingUser = await db.user.findUnique({
+        where: {email: formData.from},
+    })
+
+    if (!purchasingUser) {
+        return { error: "Du er ikke logget inn" };
+    }
+    if (purchasingUser.money - formData.price < 0) {
+        return { error: "Du har ikke nok penger!" };
+    }
+    const purchaseData: PurchaseData = await db.purchase.create({
+        data: {
+            type: formData.type,
+            text: formData.text,
+            price: formData.price,
+            to: formData.to,
+            from: formData.from,
+            createdAt: new Date()
+        }
+    })
+    await db.user.update({
+        where: {email: formData.to},
+        data: {
+            money: {
+                decrement: formData.price
+            }
+        }
+    })
+    revalidatePath("/shop")
+    return { data: purchaseData };
+
+} catch (error) {
+    return { error: "Noe gikk galt. Spør Mads" };
+}
+
+
+}
+
+export default addPurchase;
