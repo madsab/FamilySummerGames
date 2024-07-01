5b1c12af0f3d5caed704f9b544951a6689a17a40
madsab <mads.barnes@hotmail.com>
2024-07-01 15:01:53 +0200

    error message and type fixes

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,16 +1,26 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./config/auth/authOptions";
 import { redirect } from "next/navigation";
+import { User } from "@prisma/client";
 
-export const checkUser = async () => {
-    const session = await getServerSession(authOptions);
+export const checkUser: () => Promise<{
+    user?: User;
+    error?: string;
+}>= async () => {
+    try{
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            redirect("/signin");
 
-    if (!session) {
-        return redirect("/signin");
+        }
+        if (!session?.user) {
+            redirect("/signin");
+        }
+        const user = session.user as User;
+        return { user }
+
+    } catch (error) {
+        return { error: "Could not retrieve user" };
     }
-    if (!session?.user) {
-        return redirect("/signin");
-    }
 
-    return session.user;
 }
