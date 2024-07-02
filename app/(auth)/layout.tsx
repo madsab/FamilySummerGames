69430fbeb73e82c2c2574e196e69d5a13af28d7f
madsab <mads.barnes@hotmail.com>
2024-07-03 00:01:41 +0200

    add session provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../globals.css";
+import Provider from "../context/Provider";
 
 export default function SignupLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="w-full h-screen flex justify-center items-center">
-        <main className="bg-slate-600 p-6 rounded-md ">{children}</main>
+        <Provider>
+          <main className="bg-slate-600 p-6 rounded-md ">{children}</main>
+        </Provider>
       </body>
     </html>
   );
