a50fb9ef6b31ba35f632aae79b2ba4cfe61a2348
madsab <mads.barnes@hotmail.com>
2024-07-02 13:05:43 +0200

    add loading state

--- app/components/atoms/Button.tsx
+++ app/components/atoms/Button.tsx
@@ -1,23 +1,28 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import cn from "classnames";
+import { CircularProgress } from "@mui/material";
 
 interface ButtonProps {
   text: string;
   className?: string;
   onClick: () => void;
+  loading?: boolean;
 }
-const Button: FC<ButtonProps> = ({ text, onClick, className }) => {
+const Button: FC<ButtonProps> = ({ text, onClick, className, loading }) => {
   return (
     <button
-      onClick={onClick}
+      onClick={() => {
+        onClick();
+      }}
       type="button"
+      disabled={loading}
       className={cn(
-        "rounded bg-indigo-600 px-4 py-3 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+        "rounded bg-indigo-600 px-4 py-3 flex justify-center text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
         className
       )}
     >
-      {text}
+      {loading ? <CircularProgress color="inherit" size={25} /> : text}
     </button>
   );
 };
