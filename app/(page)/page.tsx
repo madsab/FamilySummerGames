8a9fefb022d4f6ded93ca2e35cba958af1823fd0
madsab <mads.barnes@hotmail.com>
2024-07-02 14:42:25 +0200

    fixed top-bar

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -23,14 +23,14 @@ export default async function Home() {
 
   return (
     <div className="w-full flex flex-col items-center space-y-3">
-      <div className="w-full bg-slate-800 flex items-center py-3 px-4">
+      <div className="w-full bg-slate-800 fixed top-0 flex items-center py-3 px-4">
         <Image src={Logo} alt="FSG Logo" width={50} height={50} />
         <p className="text-xl flex-1 flex justify-end items-center">
           <Icon icon={"fluent-emoji:coin"} />
           {balance}
         </p>
       </div>
-      <div className="pt-6 space-y-10 flex flex-col items-center">
+      <div className="pt-28 space-y-10 flex flex-col items-center">
         <p className="text-2xl">God dag, {session.user.name}</p>
         {first && second && third && <Podium first={first} second={second} third={third} />}
         <Rules />
