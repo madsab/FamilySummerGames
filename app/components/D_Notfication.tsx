fbb78f3943288133e78bd302a625f5037b07db12
madsab <mads.barnes@hotmail.com>
2024-07-03 19:23:34 +0200

    show all purchases

--- app/components/D_Notfication.tsx
+++ app/components/D_Notfication.tsx
@@ -1,11 +1,18 @@
 "use client";
-import React from "react";
+import React, { FC } from "react";
 import Button from "./atoms/Button";
 import deletePurchase from "../actions/deletePurchase";
 import { toast } from "react-toastify";
 import AlertDialog from "./organisms/AlertDialog";
+import getNotifications from "../actions/getNotifications";
+import { Purchase } from "@prisma/client";
+import { List, ListItem } from "@mui/material";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
-const D_Notfication = () => {
+interface D_NotficationProps {
+  notifications: Purchase[];
+}
+const D_Notfication: FC<D_NotficationProps> = ({ notifications }) => {
   const handleDelete = async () => {
     const isDeleted = await deletePurchase();
     if (isDeleted) {
@@ -16,10 +23,21 @@ const D_Notfication = () => {
   };
   return (
     <div className="flex flex-col items-center space-y-4">
-      <p>Notifications</p>
+      <p className="text-xl">Notifications</p>
+      <List className="space-y-2 mx-2">
+        {notifications.map((n) => {
+          return (
+            <ListItem key={n.id} className="text-md p-4 border  rounded-md bg-white text-black">
+              <Icon icon={"mingcute:notification-line"} className="size-5" />
+              {n.extra[0] ? `${n.text} -> ${n.to.split("@")[0]} ${n.extra[0]}` : `${n.text} -> ${n.to.split("@")[0]}`}
+            </ListItem>
+          );
+        })}
+      </List>
       <AlertDialog title="Are you sure?" description="This will delete all purchases ever made" onClick={handleDelete}>
         Delete all notifications
       </AlertDialog>
+      <div className="h-20"></div>
     </div>
   );
 };
