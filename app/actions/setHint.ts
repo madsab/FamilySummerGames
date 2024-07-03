0b2c555032d9bfef4b8a8c2f46ae7c64a03576e4
madsab <mads.barnes@hotmail.com>
2024-07-03 13:58:16 +0200

    error feedback

--- app/actions/setHint.ts
+++ app/actions/setHint.ts
@@ -3,17 +3,30 @@ import jsonData from "@/app/utils/hint.json";
 import { revalidatePath } from "next/cache";
 import {writeFileSync} from "node:fs";
 
-function setHint(game: string ) {;
-    const newData = jsonData.map((hint) => {
-        if (hint.active) {
-            return { ...hint, active: false };
-        } else if (hint.game === game) {
-            return { ...hint, active: true };
+async function setHint(game: string): Promise<{
+    data?: string
+    error?: string
+    }> {
+        if (game === "") {
+            return { error: "No game provided" };
         }
-        return hint;
-    })
-    writeFileSync("app/utils/hint.json", JSON.stringify(newData));
-    revalidatePath("/shop")
+        const newData = jsonData.map((hint) => {
+            if (hint.active) {
+                return { ...hint, active: false };
+            } else if (hint.game === game) {
+                return { ...hint, active: true };
+            }
+            return hint;
+            })
+
+    try {
+        writeFileSync("app/utils/hint.json", JSON.stringify(newData));
+        revalidatePath("/shop")
+        return { data: "Hint set"};
+
+    } catch (error) {
+        return { error: "Could not set hint" };
+    }
 }
 
 export default setHint;
