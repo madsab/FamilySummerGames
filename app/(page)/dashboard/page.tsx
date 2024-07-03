31a841e06212dea10772ab91bd47b5a6d7b38710
madsab <mads.barnes@hotmail.com>
2024-07-03 12:46:29 +0200

    setActive hint

--- app/(page)/dashboard/page.tsx
+++ app/(page)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import getHint from "@/app/actions/getHint";
+import D_Hint from "@/app/components/D_Hint";
 import { authOptions } from "@/lib/config/auth/authOptions";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -5,6 +7,7 @@ import React from "react";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
+  const { hint } = await getHint();
 
   if (!session?.user) {
     redirect("/login");
@@ -12,7 +15,13 @@ const Dashboard = async () => {
   if (session?.user.role !== "ADMIN") {
     return <div>Not authorized, sorry!</div>;
   }
-  return <div>Dashboard</div>;
+
+  return (
+    <div className="m-5 w-full flex flex-col items-center">
+      <p className="text-xl">Dashboard</p>
+      <D_Hint hints={hint || []} />
+    </div>
+  );
 };
 
 export default Dashboard;
