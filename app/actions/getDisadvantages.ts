c1fa2d789f12d9a5eaa9accffd6a39349ceb84b0
madsab <mads.barnes@hotmail.com>
2024-07-03 22:23:51 +0200

    lint + cleanup

--- app/actions/getDisadvantages.ts
+++ app/actions/getDisadvantages.ts
@@ -11,21 +11,17 @@ async function getDisadvantages(): Promise<{
 }> {
     try {
         const res = await db.purchase.findMany()
-        console.log("Res: ", res)
-        if (res.length === 0) {
-            return { dis: disadvantages }
-        }
+
         const disadvantagesUsed = res.reduce(
             (acc: Record<string, number>, value: Purchase) => {
                 acc[value.text] = (acc[value.text] || 0) + 1;
                 return acc;
         }, {} as Record<string, number>);
-        console.log("Used: ", disadvantagesUsed)
-        console.log("Disadvantages: ", disadvantages)
+
         const data = disadvantages.filter((disadvantage) => (
             disadvantagesUsed[disadvantage.title] < disadvantage.amount || !disadvantagesUsed[disadvantage.title]
-        ))
-        console.log("Data: ", data)
+        )) as Disadvantage[]
+
         return { dis: data}
     } catch (error) {
         return { error: "Could not retrieve disadvantages" }
